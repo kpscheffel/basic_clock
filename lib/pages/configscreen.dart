f3df3b3368d2889cd907e946a5e797a66fadf4ab
Peter Scheffel <peter@Peters-MacBook-Air-707.local>
2019-10-27 16:32:06 +0200

    isKeptOn settings are now working as required

--- lib/pages/configscreen.dart
+++ lib/pages/configscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:screen/screen.dart';
+import "dart:async";
 
 
 class ConfigScreen extends StatefulWidget {
@@ -11,16 +12,26 @@ class ConfigScreen extends StatefulWidget {
 
 class ConfigScreenState extends State<ConfigScreen> {
 
-  bool _configValue = false;
+  bool _isKeptOn = false;
 
-  ConfigScreenState() {
-    _configValue = false;//await Screen.isKeptOn;
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initPlatformState();
   }
 
+  initPlatformState() async {
+    bool keptOn = await Screen.isKeptOn;
+    setState(() {
+      _isKeptOn = keptOn;
+    });
+  }
 
   void _onChanged(bool value) {
     setState(() {
-      _configValue = value;
+      Screen.keepOn(value);      
+      _isKeptOn = value;
     }
     );
   }
@@ -43,7 +54,7 @@ class ConfigScreenState extends State<ConfigScreen> {
       ),
       Text("Alway on"),
       new Switch(
-        value: _configValue,
+        value: _isKeptOn,
         onChanged: (bool newValue) {
           _onChanged(newValue);
         },
@@ -52,4 +63,20 @@ class ConfigScreenState extends State<ConfigScreen> {
       ],)
     );
   }
+}
+
+class ScreenValue {
+  bool _value;
+
+  ScreenValue._();
+
+  static Future<ScreenValue> create() async {
+    var result = ScreenValue._();
+    await result._load();
+    return result;
+  }
+  Future<void> _load() async {
+    _value = await Screen.isKeptOn;
+  }
+  bool get value => _value;
 }
